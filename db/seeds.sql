617cd3555cfc5bcb1ae13945dca079bd6831fd47
Gtzquierdo <gtzquierdo@gmail.com>
2019-03-31 20:10:32 -0600

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BaconBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SteriodBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('VeggieBurger', false);
---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+var express = require('express');
+
+// Set handlebars
+var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+// Serve static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests
+app.listen(PORT, function() {
+    // Log server-side when our server has started
+    console.log("Server listening on: https://localhost: " + PORT);
+});
+
+
