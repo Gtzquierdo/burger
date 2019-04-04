5b8433fd90308aa0ec88722f16b9e726273bbe3d
Gtzquierdo <gtzquierdo@gmail.com>
2019-04-03 22:23:25 -0600

    gitingnore .env

--- config/connection.js
+++ config/connection.js
@@ -11,6 +11,7 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
+        port: 3306,
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
